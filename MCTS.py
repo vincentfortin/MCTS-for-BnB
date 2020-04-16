43c0bd2cf414de488fc3a9cbf25a1e14de0ea2d0
vincentfortin <vincent.fortin03@gmail.com>
2020-04-16 17:18:14 -0400

    Branching only up or down randomly

--- MCTS.py
+++ MCTS.py
@@ -4,6 +4,9 @@ import math
 import multiprocessing as mp
 import sys
 import numpy as np
+import time
+import re
+import random
 
 class Tree:
 	''' 
@@ -124,12 +127,148 @@ class Node:
 				n = Node(cur_node, cur_sol, cur_state, valid_sol)
 				self.candidates.append(n)
 
+class PolicyBranching(scip.Branchrule):
+	
+	def __init__(self, policy):
+		super().__init__()
+
+		self.policy_type = policy['type']
+		self.policy_name = policy['name']
+		if self.policy_type == 'internal':
+			self.policy = policy['name']
+
+	def branchinitsol(self):
+		self.ndomchgs = 0
+		self.ncutoffs = 0
+		self.state_buffer = {}
+		self.khalil_root_buffer = {}
+
+	def branchexeclp(self, allowaddcons):
+		candidate_vars, *_ = self.model.getPseudoBranchCands()
+		candidate_mask = [var.getCol().getLPPos() for var in candidate_vars]
+
+		print(self.model.getObjVal())
+		# print(candidate_vars)
+		print(len(candidate_vars))
+		cand = candidate_vars[0]
+
+		lb = cand.getLbLocal()
+		ub = cand.getUbLocal()
+
+
+		if random.random() > 0.5:
+			# Change upper or lower bound so that ub == lb
+			self.model.tightenVarLb(cand, ub)
+		else:
+			self.model.tightenVarUb(cand, lb)
+
+
+		result = scip.SCIP_RESULT.REDUCEDDOM
+
+        # fair node counting
+		if result == scip.SCIP_RESULT.REDUCEDDOM:
+			self.ndomchgs += 1
+		elif result == scip.SCIP_RESULT.CUTOFF:
+			self.ncutoffs += 1
+
+		return {'result': result}
+
+
 
 if __name__ == '__main__':
-	instance = sys.argv[1]
+	instance_file = sys.argv[1]
 	seed = 1
 	time_limit = 3600
 	episode = 1
+
+	# result_file = f"{args.problem}_{time.strftime('%Y%m%d-%H%M%S')}.csv"
+	instances = [{'path':instance_file,'type':'setcover'}]
+
+	branching_policies = [{'type':'testMCTS','name':'MCTS','seed':0}]
+
+	fieldnames = [
+	    'policy',
+	    'seed',
+	    'type',
+	    'instance',
+	    'nnodes',
+	    'nlps',
+	    'stime',
+	    'gap',
+	    'status',
+	    'ndomchgs',
+	    'ncutoffs',
+	    'walltime',
+	    'proctime',
+	]
+
+
+	for instance in instances:
+		print(f"{instance['type']}: {instance['path']}...")
+
+		for policy in branching_policies:
+			m = scip.Model()
+			m.setIntParam('display/verblevel', 0)
+			m.readProblem(f"{instance['path']}")
+			utilities.init_scip_params(m, seed=seed, heuristics=False, presolving=False, separating=False, conflict=False)
+			m.setIntParam('timing/clocktype', 1)  # 1: CPU user seconds, 2: wall clock time
+			m.setRealParam('limits/time', time_limit)
+
+			brancher = PolicyBranching(policy)
+			m.includeBranchrule(
+			    branchrule=brancher,
+			    name=f"{policy['type']}:{policy['name']}",
+			    desc=f"Custom PySCIPOpt branching policy.",
+			    priority=666666, maxdepth=-1, maxbounddist=1)
+
+			walltime = time.perf_counter()
+			proctime = time.process_time()
+
+
+			m.optimize()
+
+			print(m.getStatus())
+			print(m.getObjVal())
+			print(m.getDualbound())
+			print(m.getPrimalbound())
+
+			walltime = time.perf_counter() - walltime
+			proctime = time.process_time() - proctime
+
+			stime = m.getSolvingTime()
+			nnodes = m.getNNodes()
+			nlps = m.getNLPs()
+			gap = m.getGap()
+			status = m.getStatus()
+			ndomchgs = brancher.ndomchgs
+			ncutoffs = brancher.ncutoffs
+
+			# writer.writerow({
+   #              'policy': f"{policy['type']}:{policy['name']}",
+   #              'seed': policy['seed'],
+   #              'type': instance['type'],
+   #              'instance': instance,
+   #              'nnodes': nnodes,
+   #              'nlps': nlps,
+   #              'stime': stime,
+   #              'gap': gap,
+   #              'status': status,
+   #              'ndomchgs': ndomchgs,
+   #              'ncutoffs': ncutoffs,
+   #              'walltime': walltime,
+   #              'proctime': proctime,
+			# })
+
+			# csvfile.flush()
+			m.freeProb()
+
+			print(f"  {policy['type']}:{policy['name']} {policy['seed']} - {nnodes} ({nnodes+2*(ndomchgs+ncutoffs)}) nodes {nlps} lps {stime:.2f} ({walltime:.2f} wall {proctime:.2f} proc) s. {status}")
+
+
+
+
+
+
 
 	m = scip.Model()
 	# print(dir(m))
